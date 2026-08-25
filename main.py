06ab1ee2e2a05286250724df21c2ee8abfa62529
umarshakeb <umarshakeb@gmail.com>
2026-08-26 00:49:28 +0530

    Modified main.py to return root message

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import numpy as np
 
 app = FastAPI()
 
+@app.get("/", summary = "Root endpoint")
+def root():
+    '''Returns a welcome message.'''
+    return {"message": "Welcome to the inference service!"}
+
 @app.post("/predict", summary = "Predict purchase probability")
 async def predict(data: dict):
     '''Receives customer data and return purchase probability.'''
